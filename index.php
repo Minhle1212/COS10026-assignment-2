c37c25004f901900299f5b1e878c61d31226a28e
Minhle1212 <122199143+Minhle1212@users.noreply.github.com>
2024-03-29 09:45:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://kit.fontawesome.com/142309adca.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php
+        include("include/index.inc")
+    ?>
+</body>
+</html>
