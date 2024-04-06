5702eb2c764d0a3a8fe4f164deb30649f0ac183b
PolarPowah <109173362+PolarPowah@users.noreply.github.com>
2024-04-06 21:37:21 +0700

    new jobs page

--- html/jobs.php
+++ html/jobs.php
@@ -23,97 +23,72 @@
                 </p>
             </section>
 
-            <section class="jobs">
-                <div class="job">
-                    <div class="job_info">
-                        <div class="job_brief">
-                            <h2 class="job_name">Data Engineer</h2>
-                            <h3 class="job_head">Job brief</h3>
-                            <p class="job_desc"> You will use various methods to transform raw data into useful data systems. Overall, you will strive for efficiency by aligning data systems with business goals. Data engineer skills also include familiarity with several programming languages and knowledge of learning machine methods.</p>
-                            <h3 class="job_head">Key responsibilities</h3>
-                            <ul class="jobs_ul">
-                                <li>Analyze and organize raw data </li>
-                                <li>Build data systems and pipelines</li>
-                                <li>Evaluate business needs and objectives</li>
-                                <li>Interpret trends and patterns</li>
-                                <li>Conduct complex data analysis and report on results </li>
-                                <li>Prepare data for prescriptive and predictive modeling</li>
-                                <li>Build algorithms and prototypes</li>
-                            </ul>
-                            <h3 class="job_head">Skills and qualifications</h3>
-                            <h4>Essentials</h4>
-                            <ul class="jobs_ul" id="essential">
-                                <li>Previous experience as a data engineer or in a similar role</li>
-                                <li>Technical expertise with data models, data mining, and segmentation techniques</li>
-                                <li>Knowledge of programming languages  (e.g. Java and Python)</li>
-                                <li>Hands-on experience with SQL database design</li>
-                            </ul>
-                            <h4>Preferable</h4>
-                            <ul class="jobs_ul" id="preferable">
-                                <li>Great numerical and analytical skills</li>
-                                <li>Degree in Computer Science, IT, or similar field; a Master degree is a plus</li>
-                                <li>Data engineering certification (e.g IBM Certified Data Engineer)</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <aside>
-                        <div class="job_reqs">
-                            <h4>Job reference number</h4>
-                            <p>R2406</p>
-                            <h4>Salary</h4>
-                            <p>$128,250 - $180,000</p>
-                            <h4>Benefits</h4>
-                            <p>To help you stay energized, engaged and inspired, we offer a wide range of benefits including:</p>
-                            <ol class="jobs_ol">
-                                <li>Strong retirement plan, tuition reimbursement, comprehensive healthcare, support for working parents and Flexible Time Off (FTO) so you can relax, recharge and be there for the people you care about.</li> 
-                                <li>Employees are eligible for an annual discretionary bonus, and benefits including heath care, leave benefits, and retirement benefits.</li> 
-                            </ol>
-                            <h4>Applicants please fill this application form and report to Vice Chancellor of HRM</h4>
-                            <a href="apply.php">Link to form</a>
-                        </div>
-                    </aside>
-                </div>
+            <?php
 
-                <div class="job">
-                    <div class="job_info">
-                        <div class="job_brief">
-                            <h2 class="job_name">Software Engineer</h2>
+            include ("settings.php");
+            $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+            if (!$conn) {
+                echo "<p>Database connection failure</p>";
+            } else {
+
+                $query = "SELECT * FROM jobs";
+                $result = mysqli_query($conn, $query);
+
+                while ($job = mysqli_fetch_assoc($result)) { ?>
+
+                <section class="jobs">
+                    <div class="job">
+                        <div class="job_info">
+                            <div class="job_brief">
+                            <h2 class="job_name"><?= $job['name'] ?></h2>
                             <h3 class="job_head">Job brief</h3>
-                            <p class="job_desc">Builds financial technology products and services to meet the needs of Smarthouse and our clients. Develops software using a host of languages and open source technologies including Java, Python, JavaScript, C#, Cassandra, Angular, React Kubernetes, Kafka, Docker, Solr, iOS and more.</p>
+                            <p class="job_desc"><?= $job['brief'] ?></p>
                             <h3 class="job_head">Key responsibilities</h3>
                             <ul class="jobs_ul">
-                                <li>Takes ownership of projects and watch your creations be released to thousands of users.</li>
-                                <li>Works closely with senior engineers developing specific parts of the Smarthouse platforms</li>
-                                <li>Collaborates with users and the product team to design new features.</li>
-                                <li>Gains a broad understanding of the infrastructure and the capabilities of Smarthouse </li>
-                                <li>Collaborates with colleagues to build innovative products during firm-wide hackathons.</li>
+                                <?php
+                                $responsibilities = explode("\n", $job['responsibilities']);
+                                foreach ($responsibilities as $responsibility) {
+                                    echo "<li>" . $responsibility . "</li>";
+                                }
+                                ?>
                             </ul>
                             <h3 class="job_head">Skills and qualifications</h3>
                             <h4>Essentials</h4>
                             <ul class="jobs_ul" id="essential">
-                                <li>5 years of experience as a Software Engineering, Software Development Engineer, Software Engineer, or related role.</li>
-                                <li>Experience with: Data structures and algorithms; Applying design patterns,Angular, Spring, SQL, JavaScript, php5, and CSS3. </li>
-                                <li>Hands-on experience with building distributed systems and storage technologies such as Cassandra, Redis, and relational database.</li>
+                                <?php
+                                $qualifications = explode("\n", $job['skill_essential']);
+                                foreach ($qualifications as $qualification) {
+                                    echo "<li>" . $qualification . "</li>";
+                                }
+                                ?>      
                             </ul>
                             <h4>Preferable</h4>
                             <ul class="jobs_ul" id="preferable">
-                                <li>2 years of experience with: C# Programming language.</li>
-                                <li>Degree in Computer Science, IT, or similar field; a Master degree is a plus</li>
-                                <li>Experience with: Modern web application development, and Scala, Apache Spark, Azure Data Factory, Power BI.
+                                <?php
+                                $qualifications = explode("\n", $job['skill_preferable']);
+                                foreach ($qualifications as $qualification) {
+                                    echo "<li>" . $qualification . "</li>";
+                                }
+                                ?>
                             </ul>
                         </div>
                     </div>
                     <aside>
                         <div class="job_reqs">
                             <h4>Job reference number</h4>
-                            <p>R2426</p>
+                            <p><?= $job['ref_num'] ?></p>
                             <h4>Salary</h4>
-                            <p>$110,000 - $160,000</p>
+                            <p><?= $job['salary'] ?></p>
                             <h4>Benefits</h4>
                             <p>To help you stay energized, engaged and inspired, we offer a wide range of benefits including:</p>
                             <ol class="jobs_ol">
-                                <li>Strong retirement plan, tuition reimbursement, comprehensive healthcare, support for working parents and Flexible Time Off (FTO) so you can relax, recharge and be there for the people you care about.</li> 
-                                <li>Employees are eligible for an annual discretionary bonus, and benefits including heath care, leave benefits, and retirement benefits.</li> 
+                                <?php
+                                $benefits= explode("\n", $job['benefits']);
+                                    foreach ($benefits as $benefit) {
+                                        echo "<li>" . $benefit . "</li>";
+                                    }
+                                ?>
                             </ol>
                             <h4>Applicants please fill this application form and report to Vice Chancellor of HRM</h4>
                             <a href="apply.php">Link to form</a>
@@ -121,7 +96,9 @@
                     </aside>
                 </div>
             </section>
-        </div>
+            <?php }
+                mysqli_close($conn);
+            } ?>
         <?php include("../include/footer.inc");?>
     </body>
 </html>
